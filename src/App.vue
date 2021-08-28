84de181fe524378a31a3a1f06af698ae7f524650
SamStarlling <samelabarbosa.06@gmail.com>
2021-08-28 03:54:52 -0300

    inicício do projeto do site de previsão do tempo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
+import styled, {createGlobalStyle} from 'styled-components'
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'>
+  <GlobalStyle />
+    <Div>
+      <input type="text" placeHolder="...Procurando o país"/>
+    </Div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data() {
+    return {
+      api_key: '02400b3ec5b08d0c3d23a6f587e55e1f'
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+const GlobalStyle = createGlobalStyle`
+  margin: 50px;
+  box-sizing: 0;
+  padding: 0;
+`
+const Body = styled.body`
+  font-family: 'Courier New', Courier, monospace;
+`
+const Div = styled.div`
+ background-color: black;
+`
